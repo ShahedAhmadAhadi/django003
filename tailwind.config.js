1ca1b2b427d1e5e3214990a9660d0f1859420ef4
Shahed Ahmad <shaheda.a16@gmail.com>
2021-03-02 22:05:22 +0430

    purged

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['../your-django-folder/path-to-your-templates/**/*.html'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
