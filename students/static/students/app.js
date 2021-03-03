8d3b7639c60c0cdf4f586e13e88027a9bad79953
Shahed Ahmad <shaheda.a16@gmail.com>
2021-03-03 21:03:02 +0430

    refactor

--- students/static/students/app.js
+++ students/static/students/app.js
@@ -1,7 +1,11 @@
 let input = document.getElementById('search')
 let search_str = ''
+let ht = document.getElementById('wrapper').innerHTML
 input.addEventListener("keyup",async function () {
     let val = input.value
+if (input.value == '') {
+    document.getElementById('wrapper').innerHTML = ht
+}
     
 
     fetch(`http://localhost:8000/search/${val}`)
@@ -11,10 +15,10 @@ input.addEventListener("keyup",async function () {
             for (i in res){
                 document.getElementById('wrapper').innerHTML += `
                 <a href="../home/${ res[i]['roll_no'] }">
-                <div class="font-bold border-t-2 border-gray-200 p-2">
-                    <span class="w-1/6 inline-block"> ${res[i]['name']} </span>
+                <div class="font-bold border-t-2 border-gray-200 px-2 py-3">
+                    <span class="w-1/6 ml-2 inline-block"> ${res[i]['name']} </span>
                     <span class="w-1/6 inline-block">${res[i]['f/name']}</span>
-                    <span class="w-1/6 inline-block">${res[i]['phone']}</span>
+                    <span class="w-1/6 inline-block">(+93) - ${res[i]['phone']}</span>
                 </div>
             </a>`
                 // let li = show.appendChild(document.createElement('li'));
@@ -24,4 +28,17 @@ input.addEventListener("keyup",async function () {
                 // span2.appendChild(document.createTextNode(`${res[i]['f/name']}`))
             } 
         })
+        // if (!val) {
+        //     document.getElementById('wrapper').innerHTML += `
+        //     console.log('a')
+        //     {% for student in stu_lst %}
+        //             <a href="../home/{{ student.s_roll }}">
+        //                 <div class="row font-bold border-t-2 border-gray-200 px-2 py-3">
+        //                     <span class="w-1/6 ml-2 inline-block"> {{ student.s_name }} </span>
+        //                     <span class="w-1/6 inline-block">{{ student.s_father_name }}</span>
+        //                     <span>(+93) - {{ student.s_phone }}</span>
+        //                 </div>
+        //             </a>
+        //             {% endfor %}`
+        // }
 })
