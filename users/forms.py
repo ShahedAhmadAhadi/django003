5c93eac38b8807991981a838b64f3969e4f2a66c
Shahed Ahmad <shaheda.a16@gmail.com>
2021-03-04 19:15:21 +0430

    refactor

--- users/forms.py
+++ users/forms.py
@@ -6,13 +6,11 @@ class Register(UserCreationForm):
     class Meta: 
         model = User
         fields = [
-            'username', 'email'
+            'username', 'email', 'password1', 'password2'
         ]
         widgets = {
             'username': forms.TextInput(attrs={'class': 'w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none', 'placeholder': 'Username'}),
-            'email': forms.TextInput(attrs={'class': 'w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none', 'placeholder': 'E-mail'}),
-            'password': forms.TextInput(attrs={'class': 'w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none', 'placeholder': 'Password'}),
-            'Password confirmation': forms.TextInput(attrs={'class': 'w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none', 'placeholder': 'Confirm'}),
+            'email': forms.TextInput(attrs={'class': 'w-full border-b border-green-400 py-1 my-2 px-3 focus:outline-none', 'placeholder': 'E-mail'})
         }
         
 
